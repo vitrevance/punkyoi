9d3eb00cade96f0eaa217e94b56fe3394f5bd17c
CreoDen-dev <ruslaniki33@gmail.com>
2022-03-20 16:45:11 +0300

    README build.sh Makefile

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+echo "Building Release"
+echo "Fetching requirements"
+set echo off
+apt-get install premake -y -qq > /dev/null
+apt-get install libglew-dev -y -qq > /dev/null
+apt-get install libglfw3 -y -qq > /dev/null
+apt-get install libglfw3-dev -y -qq > /dev/null
+set echo on
+echo "Building"
+make config=release
+make clean
+echo "Done"
